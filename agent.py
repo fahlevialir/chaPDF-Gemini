4e415fbc1300a6f5379ade3a66dd8f27c2279f5a
Mukhammad Fahlevi Ali Rafsanjani <mukhammad.fahlevi@insignia.co.id>
2024-07-28 13:15:10 +0700

    Add files via upload

---
+++ agent.py
@@ -0,0 +1,49 @@
+import os
+# from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.embeddings import HuggingFaceEmbeddings
+
+from langchain.chains import ConversationalRetrievalChain
+from langchain.llms import OpenAI
+from langchain.document_loaders import PyPDFLoader
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import GoogleGenerativeAI
+from langchain import FAISS
+
+class Agent:
+    def __init__(self, google_api_key: str | None = None) -> None:
+        # if openai_api_key is None, then it will look the enviroment variable OPENAI_API_KEY
+        self.embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        self.llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash",google_api_key=google_api_key)
+        # self.llm = OllamaLLM(model="llama3")
+        self.chain = None
+        self.db = None
+
+    def ask(self, question: str) -> str:
+        if self.chain is None:
+            response = "Please, add a document."
+        else:
+            response = self.chain({"question": question, "chat_history": self.chat_history})
+            response = response["answer"].strip()
+            self.chat_history.append((question, response))
+        return response
+
+    def ingest(self, file_path: os.PathLike) -> None:
+        loader = PyPDFLoader(file_path)
+        documents = loader.load()
+        splitted_documents = self.text_splitter.split_documents(documents)
+
+        if self.db is None:
+            self.db = FAISS.from_documents(splitted_documents, self.embeddings)
+            self.chain = ConversationalRetrievalChain.from_llm(self.llm, self.db.as_retriever())
+            self.chat_history = []
+        else:
+            self.db.add_documents(splitted_documents)
+
+    def forget(self) -> None:
+        self.db = None
+        self.chain = None
+        self.chat_history = None
